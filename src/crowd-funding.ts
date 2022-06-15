8366d61ec6cf3816ca9e94b18d1077900dc68ec7
y3zha <yzhang0711@163.com>
2022-06-15 18:28:39 +0800

    ✨ the graph exercise

--- src/crowd-funding.ts
+++ src/crowd-funding.ts
@@ -1,51 +1,18 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
 import {
   CrowdFunding,
   CampaignLog
 } from "../generated/CrowdFunding/CrowdFunding"
-import { ExampleEntity } from "../generated/schema"
+import { CampaignLogEntity } from "../generated/schema"
 
 export function handleCampaignLog(event: CampaignLog): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+    // 看这个活动有无被创建
+  let campaign = CampaignLogEntity.load(event.params.campaignId.toString());
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if(campaign == null) {
+      campaign = new CampaignLogEntity(event.params.campaignId.toString());
+      campaign.receiver = event.params.receiver as Address;
+      campaign.goal = event.params.goal as BigInt;
+      campaign.save();
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.campaignId = event.params.campaignId
-  entity.receiver = event.params.receiver
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.isParticipate(...)
-  // - contract.newCampaign(...)
-  // - contract.numCampaigns(...)
-  // - contract.withdraw(...)
 }
